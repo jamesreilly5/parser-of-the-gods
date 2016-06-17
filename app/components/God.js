1cb7424fc9735f7ecf8bc98fb4dabaaf08bf59ea
James Reilly <reillyj33@gmail.com>
2016-06-17 11:37:05 +0100

    Capitalise output in the display
    There's probably a way to optimise the amount of function calls here but I might be changing the display later so will revisit later.

--- app/components/God.js
+++ app/components/God.js
@@ -1,13 +1,23 @@
 var React = require('react');
 var FontAwesome = require('react-fontawesome');
+var DisplayUtils = require('./../DisplayUtils');
 
 var God = React.createClass({
 	render: function() {
 		return (
             <div className='god-details col-md-4'>
-				<div><span>Name: </span><span className='pull-right'>{this.props.name}</span></div>
-                <div><span>Super Power: </span><span className='pull-right'>{this.props.superPower}</span></div>
-                <div><span>Date of death: </span><span className='pull-right'>{this.props.dateOfDeath}</span></div>
+				<div>
+                    <span>{ DisplayUtils.capitalise('Name:') }</span>
+                    <span className='pull-right'>{ DisplayUtils.capitalise(this.props.name) }</span>
+                </div>
+                <div>
+                    <span>{ DisplayUtils.capitalise('Super Power:') }</span>
+                    <span className='pull-right'>{ DisplayUtils.capitalise(this.props.superPower) }</span>
+                </div>
+                <div>
+                    <span>{ DisplayUtils.capitalise('Date of death:') }</span>
+                    <span className='pull-right'>{ DisplayUtils.capitalise(this.props.dateOfDeath) }</span>
+                </div>
             </div>
 		)
 	}
