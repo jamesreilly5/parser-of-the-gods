764ce100ccce23f4b9e2dde2d50f185b9540ea75
James Reilly <reillyj33@gmail.com>
2016-06-19 19:38:54 +0100

    Add logic to expire the cache

--- app/GodCache.js
+++ app/GodCache.js
@@ -1,19 +1,41 @@
+var CACHE_KEY = 'godCache';
+var TIMESTAMP_KEY = 'createdAt';
+var EXPIRY_TIME_IN_MILISECONDS = 3600000;
+
+var dateExpired = function(date) {
+    // 1 hour in miliseconds
+    return Math.abs(new Date() - date) > EXPIRY_TIME_IN_MILISECONDS;
+};
+
+var removeFromCache = function(cacheData, key) {
+    delete godData[key];
+    localStorage.setItem(CACHE_KEY, JSON.stringify(godData));
+};
+
 module.exports = {
+
     get: function(key) {
-        godData = JSON.parse(localStorage.getItem('godCache'));
+        godData = JSON.parse(localStorage.getItem(CACHE_KEY));
         if(!godData || !godData[key]) { return null; }
+
+        var createdAt = new Date(godData[key][TIMESTAMP_KEY]);
+        if(dateExpired(createdAt)) {
+            removeFromCache(godData, key);
+            return null;
+        }
 
         return godData[key];
     },
 
     set: function(searchKey, hash) {
-        godData = JSON.parse(localStorage.getItem('godCache'));
+        godData = JSON.parse(localStorage.getItem(CACHE_KEY));
         if(!godData) {
             godData = {};
         }
         // Assuming we will always only contain one key
         var key = Object.keys(hash)[0];
+        hash[TIMESTAMP_KEY] = new Date();
         godData[searchKey] = hash;
-        localStorage.setItem('godCache', JSON.stringify(godData))
+        localStorage.setItem(CACHE_KEY, JSON.stringify(godData))
     }
 };
--- specs/GodCache-spec.js
+++ specs/GodCache-spec.js
@@ -2,16 +2,20 @@ var GodCache = require('./../app/GodCache.js');
 
 describe('GodCache', function() {
 
-    var CACHE_KEY = 'godCache';
+    var CACHE_KEY = 'godCache',
+        mockedDate = new Date('Sun Jun 19 2016 16:00:00 GMT+0100 (IST)');
 
     describe('get', function() {
         describe('when the cache contains an entry for "foo"', function() {
             beforeEach(function() {
-                localStorage.setItem(CACHE_KEY, JSON.stringify({ foo: { bar: 'test' } }));
+                localStorage.setItem(CACHE_KEY, JSON.stringify({ foo: { bar: 'test', createdAt: '2016-06-19T15:00:00.000Z' } }));
+                spyOn(window, 'Date').and.callFake(function() {
+                  return mockedDate;
+                });
             });
 
             it('returns the entry when "foo" is requested', function() {
-                expect(GodCache.get('foo')).toEqual({ bar: 'test' });
+                expect(GodCache.get('foo')).toEqual({ bar: 'test', createdAt: '2016-06-19T15:00:00.000Z' });
             });
 
             it('returns null when a non-existent entry is requested', function() {
@@ -24,33 +28,57 @@ describe('GodCache', function() {
                 expect(GodCache.get('bar')).toEqual(null);
             });
         });
+
+        describe('when the cache contains an entry older than an hour', function() {
+            beforeEach(function() {
+                localStorage.setItem(CACHE_KEY, JSON.stringify({ foo: { bar: 'test', createdAt: '2016-06-19T15:00:00.000Z' } }));
+            });
+
+            it('returns null because it is expired', function() {
+                expect(GodCache.get('foo')).toEqual(null);
+            });
+
+            it('clears that entry in the cache because it is redundant', function() {
+                GodCache.get('foo');
+                expect(JSON.parse(localStorage.getItem(CACHE_KEY))).toEqual({});
+            });
+        });
     });
 
     describe('set', function() {
+        beforeEach(function() {
+            spyOn(window, 'Date').and.callFake(function() {
+              return mockedDate;
+            });
+        });
+
         describe('when the cache is completely empty', function() {
-            it('sets creates the cache and sets the value specified', function() {
-                GodCache.set('fizz', { some: 'data' });
-                expect(JSON.parse(localStorage.getItem(CACHE_KEY))).toEqual({ fizz: { some: 'data'} });
+            it('sets creates the cache and sets the value specified and createdAt attribute', function() {
+                GodCache.set('fizz', { some: 'data', createdAt: 'Sun Jun 19 2016 16:00:00 GMT+0100 (IST)' });
+                expect(JSON.parse(localStorage.getItem(CACHE_KEY))).toEqual({ fizz: { some: 'data', createdAt: '2016-06-19T15:00:00.000Z'} });
             });
 
             it('does not override other entries in the cache', function() {
-                GodCache.set('fizz', { some: 'data' });
-                GodCache.set('buzz', { some: 'other data data' });
+                GodCache.set('fizz', { some: 'data', createdAt: '2016-06-19T15:00:00.000Z' });
+                GodCache.set('buzz', { some: 'other data data', createdAt: '2016-06-19T15:00:00.000Z' });
                 expect(JSON.parse(localStorage.getItem(CACHE_KEY))).toEqual(
-                    { fizz: { some: 'data'}, buzz: { some: 'other data data' } }
+                    {
+                        fizz: { some: 'data', createdAt: '2016-06-19T15:00:00.000Z' },
+                        buzz: { some: 'other data data', createdAt: '2016-06-19T15:00:00.000Z' }
+                    }
                 );
             });
         });
 
         describe('when the cache already contains an entry', function() {
             beforeEach(function() {
-                localStorage.setItem(CACHE_KEY, JSON.stringify({ more: { data: 'to test' } }));
+                localStorage.setItem(CACHE_KEY, JSON.stringify({ more: { data: 'to test', createdAt: '2016-06-19T15:00:00.000Z' } }));
             });
 
             it('does not overide the existing entry', function() {
                 GodCache.set('buzz', { some: 'other data data' });
                 expect(JSON.parse(localStorage.getItem(CACHE_KEY))).toEqual(
-                    { more: { data: 'to test' }, buzz: { some: 'other data data' } }
+                    { more: { data: 'to test', createdAt: '2016-06-19T15:00:00.000Z' }, buzz: { some: 'other data data', createdAt: '2016-06-19T15:00:00.000Z' } }
                 );
             });
         });
@@ -59,7 +87,5 @@ describe('GodCache', function() {
     afterEach(function() {
         localStorage.setItem(CACHE_KEY, null);
     });
-
-    // TODO: Expire cache
 
 });
